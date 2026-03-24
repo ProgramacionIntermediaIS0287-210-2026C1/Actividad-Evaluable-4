9ee0a8efeb9220deb5902fd5713f1767e8a322da
Gedots <gotalora7@gmail.com>
2026-03-24 23:56:20 +0000

    Clase Value Objects

---
+++ Actividad-Evaluable-4/src/com/exam/domain/vo/ValueOjects.java
@@ -0,0 +1,20 @@
+  public class ValueObjects {
+
+    public record StudentId(String value) {
+        public StudentId {
+            if (value == null || value.isBlank()) {
+                throw new IllegalArgumentException("El ID del estudiante no puede estar vacio");
+            }
+        }
+    }
+    
+  public record QuestionId(String value) {
+  }
+
+  public record AnswerText(String value) {
+  }
+
+  public record Calificacion(int puntaje, int total) {
+  }
+
+  }
