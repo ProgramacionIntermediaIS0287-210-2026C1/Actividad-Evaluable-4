3cd83d32b90cabc8873d73b162f84c8ef31b4e5f
andersonsua0331-rgb <andersonsua0331@gmail.com>
2026-04-03 17:38:48 +0000

    arreglos de codigo

--- src/com/exam/domain/model/QuestionTypes.java
+++ src/com/exam/domain/model/QuestionTypes.java
@@ -8,7 +8,7 @@ public class QuestionTypes{
     public static class  singleChoiceQuestion extends Question {
     private final List<String> options;
 
-    public singleChoiceQuestion(Question id, String text List<String> options, AnswerText correct){
+    public singleChoiceQuestion(Question id, String text, List<String> options, AnswerText correct){
         super(id, text, correct);
         this.options = options;
 
@@ -20,11 +20,12 @@ public class QuestionTypes{
     }
 
     @Override
-    public abstract void displayFormat(){
+    public void displayFormat(){
         System.out.println("[Unica respuesta]"+ text);
-        options.forEach(o-> System.out.println("()"+ o));
+        options.forEach(o-> System.out.println("()" + o));
     }
 
     }
+     
 
 }
