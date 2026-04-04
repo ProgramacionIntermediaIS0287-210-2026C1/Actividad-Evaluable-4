5cb97740dc78b3c4a88610724d7f0e04bd3380ec
Mariana <mvreyesp@libertadores.edu.co>
2026-04-04 00:15:58 +0000

    memo

--- src/com/exam/infrastructure/InMemoryExamAttemRepository.java
+++ src/com/exam/infrastructure/InMemoryExamAttemRepository.java
@@ -4,10 +4,7 @@ class InMemoryExamAttemptRepository {
 
     private Map<String, ExamAttempt> db = new HashMap<>();
 
-    public Optional<ExamAttempt> findActiveByStudent(String studentId) {
-        ExamAttempt attempt = db.get(studentId);
-        if (attempt != null && !attempt.estaFinalizado()) {
-            return Optional.of(attempt);
+    
         }
         return Optional.empty();
     }
