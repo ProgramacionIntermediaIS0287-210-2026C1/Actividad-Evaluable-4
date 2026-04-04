624baf6c6eb9c5f77ffd256217a041c60b0b27b0
Mariana <mvreyesp@libertadores.edu.co>
2026-04-04 08:34:32 +0000

    es

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -4,13 +4,6 @@ public class Estudiante {
     private String nombre;
 
     public Estudiante(String id, String nombre) {
-        if (id == null || id.isBlank()) {
-            throw new RuntimeException("ID inválido");
-        }
-        if (nombre == null || nombre.isBlank()) {
-            throw new RuntimeException("Nombre inválido");
-        }
-
         this.id = id;
         this.nombre = nombre;
     }
@@ -21,10 +14,5 @@ public class Estudiante {
 
     public String getNombre() {
         return nombre;
-    }
-
-    @Override
-    public String toString() {
-        return nombre + " (" + id + ")";
     }
 }
